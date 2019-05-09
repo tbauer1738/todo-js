b624a40e6836ed42f7e07f918d7c3d5f762e6cfa
tjbq53 <tom.bauer95@gmail.com>
2019-05-08 21:47:20 -0500

    Update todo.js to load existing items in list

--- script/todo.js
+++ script/todo.js
@@ -5,66 +5,19 @@ let todos = [
   {id: 4, text: "Write thank-you notes", complete: false},
 ];
 
-//document.getElementById("demo").innerHTML = todos;
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var fruits = ["Banana", "Orange", "Apple", "Mango"];
-  fruits.toString();
-  document.getElementById("demo").innerHTML = fruits;
-}, false);
-
-
-
-//User adds a new todo, text from field turns into a todo
-document.getElementById('add').addEventListener('click', function () {
-
-  var value = document.getElementById('item').value;
-  if (value) addItemTodo(value); 
-  });
-
-
-
-
-function loadDefaultTasks(){
-
-  for(var i=0;i<todos.length;i++){
-    document.write("<b>todos["+i+"] is </b>=>"+todos[i]+"<br>");
-    }
-
-
+//establishing the prepopulated list of todos 
+function loadTodos(){
+  var i;
+  for (var i=0; i < todos.length; i++){
+	document.getElementById('main-todo-list').innerHTML += '<div class="todo"><input type="checkbox" class="todo-checkbox" /><span class="todo-text">' + todos[i].text + '</span></div>';
+  }
 }
 
 
-//*Saving below for when I move on to adding new tasks*/
+//triggering function to load propulated list of todos on page
+window.addEventListener('load', function (){
+  loadTodos();
+});
 
-  // function addItemTodo(text){
-  //   var list = document.getElementById('main-todo-list');
-
-  //   var div = document.createElement('div')
-  //   div.setAttribute("class", "todo");
-  //   div.setAttribute("id", "todo-entry");
-    
-  //   var item = document.createElement('span');
-  //   item.setAttribute("class", "todo-text");
-  //   item.innerHTML = text;
-
-  //   var checkbox = document.createElement('input');
-  //   checkbox.setAttribute("type", "checkbox");
-  //   checkbox.setAttribute("class", "todo-checkbox");
-  //   checkbox.setAttribute("id", "complete")
-
-  //   var br = document.createElement("br");
-  //   item.appendChild(br);
-
-  //   document.getElementById('main-todo-list').appendChild(div);
-  //   document.getElementById('todo-entry').appendChild(checkbox);
-  //   document.getElementById('todo-entry').appendChild(item);
-   
-    
-
-  //   //div.insertBefore(item, list);
-  //   list.insertBefore(checkbox, item);
-  // }
 
 
